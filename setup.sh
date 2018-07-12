d18dd3322b641985718daad7488d838a490cfed4
Corey <corey_snodgrass@wycliffe.org>
2018-07-12 19:36:19 -0400

    WIP:add cp instead of mv for setup files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # run from inside mern-discovery after first clone
-mv .xinitrc ../.xinitrc
-mv .browse ../.browse
+cp .xinitrc ../.xinitrc
+cp .browse ../.browse
 chmod a+x ../.browse
 chown alarm:alarm ../.browse

20427f6d6125198529522615dfe98041fc7b7dd4
Corey <corey_snodgrass@wycliffe.org>
2018-07-12 19:40:35 -0400

    WIP:add npm install

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ cp .xinitrc ../.xinitrc
 cp .browse ../.browse
 chmod a+x ../.browse
 chown alarm:alarm ../.browse
+
+# run npm install on both directories
+npm install
+cd client/
+npm install
+cd ..
